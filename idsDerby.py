98cac83615a7d6b93cce6b1640611b6754efe445
Octave-L <octave.laurans@gmail.com>
2022-03-17 20:18:22 +0100

    vala

--- idsDerby.py
+++ idsDerby.py
@@ -1,11 +1,2 @@
 import requests
-url = "https://opendomesday.org/api/1.0/county/"
-r = requests.get(url)
-data = r.json()
-for i in data:
-    if i.get('name') == "Derbyshire":
-        place = i.get("places_in_county")
-        for j in place:
-           print(j.get("id"))
-
-
+[[print(j.get("id")) for j in i.get("places_in_county")] for i in requests.get("https://opendomesday.org/api/1.0/county/").json() if i.get('name') == "Derbyshire"]
